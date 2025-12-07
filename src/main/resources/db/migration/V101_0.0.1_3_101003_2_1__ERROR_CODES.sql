50f936a7422eaa6423155a8c4464fb6788578f8d
Khabilan <64898963+Khabilan1003@users.noreply.github.com>
2025-12-07 12:13:46 +0530

    Error Message Change

--- src/main/resources/db/migration/V101_0.0.1_3_101003_2_1__ERROR_CODES.sql
+++ src/main/resources/db/migration/V101_0.0.1_3_101003_2_1__ERROR_CODES.sql
@@ -2,7 +2,7 @@ INSERT INTO error_codes (code, message) VALUES
 ('UD_001', 'Invalid Short URL');
 
 INSERT INTO error_codes (code, message) VALUES
-('UD_002' , 'The given short url {0} is expired');
+('UD_002' , 'The given short url is expired');
 
 INSERT INTO error_codes (code, message) VALUES
 ('UD_003' , 'Invalid Original URL');
